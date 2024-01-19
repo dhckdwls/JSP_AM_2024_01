3ccbcaa1d96b3df863493a060fdb34024ac56eee
dhckdwls <codetama51@gmail.com>
2024-01-19 17:39:19 +0900

    파라미터에 색상 추가 (스스로)

--- src/main/java/com/KoreaIT/java/Jsp_AM/printDanServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/printDanServlet.java
@@ -1,6 +1,7 @@
 package com.KoreaIT.java.Jsp_AM;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,11 +14,17 @@ public class printDanServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		//printDan?dan=8&limit=2
+		//8단출력 * 2
+		//printDan?dan=8&limit=2&color=red
+		//색상은 레드로
 		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
 
 		String inputedDan = request.getParameter("dan");
 		String inputedLimit = request.getParameter("limit");
+		String inputedColor = request.getParameter("color");
+		
 
 		if (inputedDan == null) {
 			inputedDan = "1";
@@ -25,16 +32,33 @@ public class printDanServlet extends HttpServlet {
 		if (inputedLimit == null) {
 			inputedLimit = "1";
 		}
-
-
+		if(inputedColor == null) {
+			inputedColor = "black";
+		}
+		
+		String color = inputedColor;
+//		<div style="color:red;">asd</div>
+//		<div style=\"color:red;\">asd</div>
+		out.print("<html>");
+		out.print("<body>");
+		out.print("<div style=\"color:"+color+";\">");
+		
+		
 		int dan = Integer.parseInt(inputedDan);
 		int limit = Integer.parseInt(inputedLimit);
-
-		response.getWriter().append(String.format("==%d단==<br>", dan));
-
-		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		
+		int startNumber = 1;
+		while (startNumber <= limit) {
+			response.getWriter().append(String.format("==%d단==<br>", dan));
+			for (int i = 1; i <= 9; i++) {
+				response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+			}
+			startNumber++;
 		}
+		out.print("</div>");
+		out.print("</body>");
+		out.print("</html>");		
+		
 	}
 
 }
